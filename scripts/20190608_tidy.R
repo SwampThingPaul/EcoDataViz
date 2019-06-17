9946b8756ceb1b0665da2d0e6755905afb65178b
SwampThingPaul <pjulian@ufl.edu>
2019-06-17 17:48:54 -0400

    edits

--- scripts/20190608_tidy.R
+++ scripts/20190608_tidy.R
@@ -11,6 +11,8 @@ rm(list=ls(all=T));cat("\014");dev.off()
 library(AnalystHelper);#devtools::install_github("SwampThingPaul/AnalystHelper")
 library(plyr)
 library(reshape)
+library(RColorBrewer)
+library(classInt)
 
 #Paths
 setwd("D:/_GitHub/PseudoTidyTuesday")
@@ -90,14 +92,6 @@ dev.off()
 
 
 # additional analyses -----------------------------------------------------
-test.rslt=data.frame()
-test=subset(dat,siteID==longterm.sites$siteID[1])
-for(j in 1:length(yrs)){
-
-}
-
-plot(DOY~CY,test.rslt)
-
 seasonal.peak=data.frame()
 for(i in 1:length(longterm.sites$siteID)){
   tmp.dat=subset(dat,siteID==longterm.sites$siteID[i])
@@ -113,11 +107,43 @@ for(i in 1:length(longterm.sites$siteID)){
     }
   }
 }
+ddply(seasonal.peak,"siteID",summarise,kendall=cor.test(CY,DOY,method="kendall")$estimate,pval=cor.test(CY,DOY,method="kendall")$p.val)
 
+## Plot
+n=6
+pal <- rev(brewer.pal(n, "BuGn"))
+pt.cex.vals=rev(seq(1,2.5,l=n))
+int=classIntervals(seasonal.peak$max.val,n,style="fixed",fixedBreaks=c(0.2,5,10,20,100,500,1500))
+site.labs=paste(unlist(stringr::str_split(longterm.sites$name," Beach "))[seq(1,14,2)],"Beach")
+
+ylim.val=c(120,300);by.y=30;ymaj=seq(ylim.val[1],ylim.val[2],by.y);ymin=seq(ylim.val[1],ylim.val[2],by.y/2)
+xlim.val=c(2005,2019);by.x=4;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/by.x)
+#tiff(filename=paste0(plot.path,"tiff/20190617_iowaDNR_Peakmycrosystin.tiff"),width=7,height=4,units="in",res=200,type="windows",compression=c("lzw"),bg="white")
+#png(filename=paste0(plot.path,"png/20190617_iowaDNR_Peakmycrosystin.png"),width=7,height=4,units="in",res=200,type="windows",bg="white")
 par(family="serif",mar=c(1.5,2.25,0.5,0.1),oma=c(2,1.75,0.7,1));
 layout(matrix(1:8,2,4,byrow=T))
+
 for(i in 1:7){
-plot(DOY~CY,subset(seasonal.peak,siteID==longterm.sites$siteID[i]))
+  tmp.dat=subset(seasonal.peak,siteID==longterm.sites$siteID[i])
+  plot(DOY~CY,tmp.dat,axes=F,ylab=NA,xlab=NA,ylim=ylim.val,xlim=xlim.val,type="n")
+  abline(h=ymaj,v=xmaj,lty=3,col="grey")
+  col=pal[findInterval(tmp.dat$max.val,int$brks,all.inside = T)]
+  pt.cex.grad=pt.cex.vals[findInterval(tmp.dat$max.val,int$brks,all.inside = T)]
+  with(tmp.dat,lines(CY,DOY,lwd=1.75,col="grey"))
+  with(tmp.dat,points(CY,DOY,pch=21,bg=col,cex=pt.cex.grad,lwd=0.1))
+  if(i%in%c(1:3)){axis_fun(1,line=-0.5,xmaj,xmin,NA)}else{axis_fun(1,line=-0.5,xmaj,xmin,xmaj)}
+  if(i%in%c(1,5)){axis_fun(2,ymaj,ymin,ymaj)}else{axis_fun(2,ymaj,ymin,NA)};box(lwd=1)
+  mtext(side=3,site.labs[i],cex=0.6)
 }
 
-ddply(seasonal.peak,"siteID",summarise,kendall=cor.test(CY,DOY,method="kendall")$estimate,pval=cor.test(CY,DOY,method="kendall")$p.val)
+plot(0:1,0:1,type = 'n', axes = F,xlab=NA, ylab=NA)
+labs=NA
+mc.conc=c(0.2,5,10,20,100,500,1500)
+for(i in 1:n){labs[i]=paste(mc.conc[i],mc.conc[i+1],sep=" - ")}
+labs[1]="< 5"
+legend(0.5,0.5,legend=rev(labs),pch=c(21),pt.bg=pal,lwd=0.1,lty=NA,pt.cex=pt.cex.vals,ncol=1,cex=1,bty="n",y.intersp=1,x.intersp=0.75,xpd=NA,xjust=0.5,yjust=0.5,
+       title="Peak Total Microcystin\nConcentration(\u03BCg L\u207B\u00B9)")
+text(1,0.1,adj=1,font=3,cex=0.5,"Data from Iowa DNR\n(https://programs.iowadnr.gov/aquia/search)\n most recent data up to June 17, 2019")
+mtext(side=2,line=0.5,outer=T,"Day of Year Peak Microcystin Conc.")
+mtext(side=1,line=0.5,outer=T,"Calendar Year")
+dev.off()
